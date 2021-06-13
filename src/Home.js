cc062e9d295703a75349ccaa993e220aa5e2ba25
AaA <67450869+Awara-Amin@users.noreply.github.com>
2021-06-13 12:50:04 +0300

    Add files via upload

---
+++ src/Home.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaBars } from 'react-icons/fa'
+// import {AppContext, useGlobalContext} from './context'
+import {useGlobalContext} from './context'
+
+
+const Home = () => {
+  // const data = useContext(AppContext);
+  // const data = useGlobalContext();
+  const { openSidebar, openModal } = useGlobalContext();
+
+  // console.log(data)
+
+  return (
+    <main>
+      <button className='sidebar-toggle' onClick={openSidebar}>
+        <FaBars />
+      </button>
+      <button className='btn'            onClick={openModal}>show modal</button>
+    </main>
+  );
+};
+
+export default Home
